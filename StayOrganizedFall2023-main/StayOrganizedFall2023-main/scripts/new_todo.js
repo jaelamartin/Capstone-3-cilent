06f7a49ff90b381106e759fd43645e671d2c8e31
Jaela Martin <jmartin@appdev.yearup.org>
2023-12-08 17:17:30 -0500

    first commit

---
+++ StayOrganizedFall2023-main/StayOrganizedFall2023-main/scripts/new_todo.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", e => {
+
+    fillUserList();
+
+    const catOption = c => `<option>${c.name}</option>`;
+    //REMOVE VALUE SO THE DISPLAY NAME WILL BE USED BY THE NEW TODO
+    // const catOption = c => `<option value="${c.id}">${c.name}</option>`;
+
+    fetch("http://localhost:8083/api/categories")
+        .then(r => r.json())
+        .then(cats => cats.forEach(c => catList.innerHTML += catOption(c)));
+
+    //// GOAL IS TO GENERATE AN ENCODED FORM TO SEND AS THE "body"    
+    //userid=1&category=1&priority=Medium&description=Make+Tea&deadline=2023-12-06
+
+    saveButton.addEventListener("click", e => {
+
+        // LOOP WAY OF GATHERING FORM DATA
+        const elements = document.querySelectorAll("main [name]");
+        const content = [...elements].map(e => `${encodeURIComponent(e.name)}=${encodeURIComponent(e.value)}`).join("&");
+
+        // LINEAR WAY OF GATHERING FORM DATA
+        // const content = new FormData();
+        // content.append("userid", userList.value);
+        // content.append("category", catList.value);
+        // content.append("priority", priorityList.value);
+        // content.append("description", description.value);
+        // content.append("deadline", deadline.value);
+        // console.log(content)
+        //// FormData() NOT WORKING WITH ANY OR NONE FOR CONTENT TYPE
+
+        fetch("http://localhost:8083/api/todos/", {
+            method: "POST", body: content,
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded" //"application/json"
+            }
+        }).then(r => {
+            location="thank_you.html"
+        });
+
+    }); //END SAVE
+});//END LOADED
---
+++ StayOrganizedFall2023-main/StayOrganizedFall2023-main/scripts/todos.js
@@ -0,0 +1,27 @@
+function todo(t) {
+    // const completed = t.completed ? "[X]" : "[&nbsp;]";
+    // let completed = "[&nbsp;]"
+    // if(t.completed){
+    //     completed = "[X]"
+    // }
+    const completed = `<img src="./images/check_box_${t.completed}.svg">Completed`
+    return `
+    <div class="todo card">
+        <div class="id">         TODO ID:     <b>${t.id}</b></div>
+        <div class="userid">     USER ID:     <b>${t.userid}</b></div>
+        <div class="category">   CATEGORY:    <b>${t.category}</b></div>
+        <div class="description">DESCRIPTION: <b>${t.description}</b></div>
+        <div class="deadline">   DEADLINE:    <b>${t.deadline}</b></div>
+        <div class="priority">   PRIORITY:    <b>${t.priority}</b></div>
+        <button class="completed" onclick="mark(${t.id})">  ${completed}</button>
+    </div>
+    `;
+}
+function draw() {
+    fetch(host + "/api/todos/")
+        .then(r => r.json())
+        .then(todos => {
+            todoList.innerHTML = todos.filter(t => t.userid == userList.value).map(todo).join("");
+        }
+
+    }
